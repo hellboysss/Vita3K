bed28af87a59a2a9ea2e159a6364ed7ba6f7dd04
pent0 <fewdspuckrito@gmail.com>
2019-06-12 10:22:42 +0700

    emulator: Draw trophies unlock window

--- src/emulator/main.cpp
+++ src/emulator/main.cpp
@@ -105,7 +105,7 @@ int main(int argc, char *argv[]) {
         app::error_dialog("Host initialisation failed.", host.window.get());
         return HostInitFailed;
     }
-
+    
     gui::init(host);
 
     auto discord_rich_presence_old = host.cfg.discord_rich_presence;
@@ -149,6 +149,8 @@ int main(int argc, char *argv[]) {
         gl_renderer.render(host);
         gui::draw_begin(host);
         gui::draw_common_dialog(host);
+        gui::draw_trophies_unlocked(host);
+        
         if (host.display.imgui_render) {
             gui::draw_ui(host);
         }

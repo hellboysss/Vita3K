65f761ec7fa36e5f93b3cd8933abd7ae386608cb
1whatleytay <1whatleytay@hdsb.ca>
2020-03-28 15:27:50 -0400

    reverb tries

--- vita3k/ngs/src/modules/reverb.cpp
+++ vita3k/ngs/src/modules/reverb.cpp
@@ -18,7 +18,17 @@ namespace ngs::reverb {
 
         auto *params = voice->get_parameters<Parameters>(mem);
 
-        LOG_INFO(":thinking:");
+        assert(voice->inputs.inputs.size() == 1);
+        assert(voice->inputs.inputs[0].bufs.size() == 1);
+        uint8_t *output_data = voice->inputs.inputs[0].bufs[0].data();
+
+        constexpr uint32_t channels = 2; // fix later
+        constexpr uint32_t frequency = 48000; // fix later
+        const uint32_t samples = voice->inputs.inputs[0].bufs[0].size() / sizeof(uint16_t) / channels;
+
+        if (!route(mem, voice, nullptr, channels, samples, frequency, AudioDataType::S16)) {
+            route(mem, voice, output_data, channels, samples, frequency, AudioDataType::S16);
+        }
 
         // TODO: actually implement reverb, plan is to pass through for now
     }
